a9b264fd50d00d993e02fcbc0668be325be01055
SkyKingPX <skykingpx.dev@gmail.com>
2025-11-01 12:33:35 +0100

    Tiny fix ig

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,15 @@
 plugins {
     id("java")
-    application
+    id("application")
     id("com.gradleup.shadow") version "8.3.1"
 }
 
-application.mainClass = "de.skyking_px.PhoenixBot.Bot"
 group = "de.skyking_px"
-version = "2.0.0-rc6"
+version = "2.0.0-rc7"
+
+application {
+    mainClass.set("de.skyking_px.PhoenixBot.Bot")
+}
 
 repositories {
     mavenCentral()
@@ -18,8 +21,8 @@ dependencies {
     implementation("net.dv8tion:JDA:6.1.0")
     implementation("org.yaml:snakeyaml:2.5")
     implementation("ch.qos.logback:logback-classic:1.5.19")
-    implementation ("org.apache.httpcomponents.client5:httpclient5:5.2")
-    implementation ("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+    implementation("org.apache.httpcomponents.client5:httpclient5:5.2")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
 }
 
 tasks.test {
@@ -29,6 +32,5 @@ tasks.test {
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
     options.isIncremental = true
-
     sourceCompatibility = "21"
 }
